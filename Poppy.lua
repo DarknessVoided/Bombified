68e205998ac9c514762087b5d53daf7ac8f3ee3f
DarknessVoided <darknessvoided@icloud.com>
2015-09-12 20:50:19 +0800

    updated channel spell

--- Poppy.lua
+++ Poppy.lua
@@ -4,14 +4,14 @@ require("MapPositionGOS")
 require("IOW")
 require("Inspired")
 
----------------------Main Menu------------------
+--------Main Menu--------
 PoppyMenu = Menu("Poppy", "Poppy")
-------------------------------------------------
+------------------------
 
----------------------Sub Menu-------------------
+--------Sub Menu-------
 PoppyMenu:SubMenu("Combo", "Combo")
 PoppyMenu:SubMenu("Misc", "Misc")
-------------------------------------------------
+------------------------
 
 
 
@@ -27,5 +27,20 @@ PoppyMenu.Misc:Boolean("ER", "Use E to interupt enemy Ulti", true)
 --------------------------
 
 CHANELLING SPELLS = {
-     ["Caitlyn"]      = {_R},
+    ["Caitlyn"]                     = {_R},
+    ["Katarina"]                    = {_R},
+    ["MasterYi"]                    = {_W},
+    ["FiddleSticks"]                = {_W, _R},
+    ["Galio"]                       = {_R},
+    ["Lucian"]                      = {_R},
+    ["MissFortune"]                 = {_R},
+    ["VelKoz"]                      = {_R},
+    ["Nunu"]                        = {_R},
+    ["Shen"]                        = {_R},
+    ["Karthus"]                     = {_R},
+    ["Malzahar"]                    = {_R},
+    ["Pantheon"]                    = {_R},
+    ["Warwick"]                     = {_R},
+    ["Xerath"]                      = {_R},
+
 }
