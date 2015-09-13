0ba72e15ef9a196c0adee96c32ba2875ed433af1
DarknessVoided <darknessvoided@icloud.com>
2015-09-13 21:52:53 +0800

    Update Trundle.lua

--- Trundle.lua
+++ Trundle.lua
@@ -13,8 +13,6 @@ TrundleMenu:SubMenu("Miscva", "Misc")
 TrundleMenu:SubMenu("Drawings", "Drawings")
 --------------------------
 
-
-
 --Menu within combo Menu--
 TrundleMenu.Combo:Boolean("Q", "Use Q", true)
 TrundleMenu.Combo:Boolean("W", "Use W", true)
@@ -25,19 +23,19 @@ TrundleMenu.Combo:Boolean("R", "Use R", true)
 -----Menu within Misc-----
 TrundleMenu.Misc:Boolean("ER", "Use E to interupt enemy Ulti", true)
 TrundleMenu.Misc:Boolean("QE", "Auto level Spell in QWER", false)
---------------------------
 
----Menu within Drawings---
+-----------------------------------------------------------Menu within Drawings----------------------------------------------
 TrundleMenu.Drawings:Boolean("Q", "Enable Drawings for Q", false)
 TrundleMenu.Drawings:Boolean("W", "Enable Drawings for W", false)
 TrundleMenu.Drawinfs:Boolean("E", "Enable Drawings for E", false)
 TrundleMenu.Drawings:Boolean("R", false)
-----All my Local stuff----
+-----------------------------------------------------------All my Local stuff------------------------------------------------
 local myHero = GetMyHero();
 local CannotHitMe = IsImmune()
 local isTargetable = isTargetable()
-
---------------------------
+local unitChanellingSpells = CHANELLING_SPELLS[GetObjectName(unit)]
+local callback = Nil
+-----------------------------------------------------------------------------------------------------------------------------
 CHANELLING_SPELLS = {
     ["Caitlyn"]                     = {_R},
     ["Katarina"]                    = {_R},
@@ -56,17 +54,16 @@ CHANELLING_SPELLS = {
     ["Xerath"]                      = {_R},
     ["Tristana"]                      = {_W},
 }
+-----------------------------------------------------------------------------------------------------------------------------
 
-local callback = nil
- ------------------------------Interrupter Part------------------------------
-OnProcessSpell(function(unit, spell)    
+---------------------------------------------All those interrupter   code goes under here------------------------------------
+-OnProcessSpell(function(unit, spell)    
     if not callback or not unit or GetObjectType(unit) ~= Obj_AI_Hero  or GetTeam(unit) == GetTeam(GetMyHero()) then return end
-    local unitChanellingSpells = CHANELLING_SPELLS[GetObjectName(unit)]
  
-        if unitChanellingSpells then
+    if unitChanellingSpells then
             for _, spellSlot in pairs(unitChanellingSpells) do
                 if spell.name == GetCastName(unit, spellSlot) then callback(unit, CHANELLING_SPELLS) end
             end
         end
------------------------------------------------------------------------------
-end) ---Ends the OnLoop
+-----------------------------------------------------------------------------------------------------------------------------
+end)
