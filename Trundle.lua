2e121677141c9e73b8885955f568d297f01aa268
DarknessVoided <darknessvoided@icloud.com>
2015-09-16 20:05:44 +0800

    Update Trundle.lua

--- Trundle.lua
+++ Trundle.lua
@@ -31,6 +31,7 @@ local myHero = GetMyHero();
 local isTargetable = isTargetable(target)
 local unitChanellingSpells = CHANELLING_SPELLS[GetObjectName(unit)]
 local callback = Nil
+local target = GetCurrentTarget()
 AutoLevel()
 Combo()
 local botrk = GetItemSlot(myHero,3153)
