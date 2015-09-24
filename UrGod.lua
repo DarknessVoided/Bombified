d4d1d9374687d91f15634f7d3a98efee5dac8b3c
DarknessVoided <darknessvoided@icloud.com>
2015-09-24 20:05:10 +0800

    Update UrGod.lua

--- UrGod.lua
+++ UrGod.lua
@@ -37,8 +37,7 @@ else
 end
 
 --Use Q
-
-local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1800,250,1050,80,false,true)
+local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1800,250,1050+extraRange,80,collision,true)
 
 if CanUseSpell(myHero,_Q) == READY and GoS:ValidTarget(target,1000+extraRange) and Urgot.Combo.Q:Value() and Urgot.Combo.Combo1:Value() and if QPred.HitChance == 1 then
 CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,Qpred.PredPos.z)
