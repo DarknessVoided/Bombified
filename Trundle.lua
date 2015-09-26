3bb17db3ad253c2094c8158bf94d2f5af058318e
DarknessVoided <darknessvoided@icloud.com>
2015-09-26 22:43:40 +0800

    Update Trundle.lua

--- Trundle.lua
+++ Trundle.lua
@@ -1,4 +1,4 @@
-if GetObjectName(myHero) ~= "Trundle" then return end
+if GetObjectName(myHero) ~= "Trundle"  then else return end
 
 require("IOW")
 require("Inspired")
@@ -20,12 +20,20 @@ TrundleMenu.Combo:Boolean("R", "Use R", true)
 TrundleMenu.Misc:Boolean("ER", "Use E to interupt enemy channels", true)
 TrundleMenu.Misc:Boolean("QE", "Auto level Spell in RQWE", false)
 TrundleMenu.Misc:Boolean("WQ", "Auto Level Spell in RWQE", false)
-
----------------------------------------------------All my stuff -------------------------------------------
-myHero = GetMyHero()
-target = GetCurrentTarget()
----------------------------------------------All those interrupter code goes under here------------------------------------
-
+-----------------------------------------------------------Menu within Drawings----------------------------------------------
+TrundleMenu.Drawings:Boolean("Q", "Enable Drawings for Q", false)
+TrundleMenu.Drawings:Boolean("W", "Enable Drawings for W", false)
+TrundleMenu.Drawings:Boolean("E", "Enable Drawings for E", false)
+TrundleMenu.Drawings:Boolean("R", false)
+---------------------------------------------------All my Local stuff -------------------------------------------
+OnLoop(function(myHero)
+local isTargetable = isTargetable(target)
+local unitChanellingSpells = CHANELLING_SPELLS[GetObjectName(unit)]
+local target = IOW:GetTarget()
+AutoLevel()
+Combo()
+end)
+-----------------------------------------------------------------------------------------------------------------------------
 CHANELLING_SPELLS = {
     ["Caitlyn"]                     = {_R},
     ["Katarina"]                    = {_R},
@@ -45,57 +53,22 @@ CHANELLING_SPELLS = {
     ["Tristana"]                    = {_W},
     ["Vi"]                          = {_Q}
 }
-
-local unitChanellingSpells = CHANELLING_SPELLS[GetObjectName(unit)]
-local playerTeam = GetTeam(GetMyHero()) --Because of line 25 there isn't any need to do local playerTeam = GetTeam(GetMyHero())
-
-OnProcessSpell(function(unit, spell)    --Starts of OnProcessSpell-Notice that the ) has not been closed.
-    if not unit or GetObjectType(unit) ~= Obj_AI_Hero  or GetTeam(unit) == playerTeam then return end
+---------------------------------------------All those interrupter   code goes under here------------------------------------
+OnProcessSpell(function(unit, spell)    
+    if not unit or GetObjectType(unit) ~= Obj_AI_Hero  or GetTeam(unit) == GetTeam(GetMyHero()) then return end
  
     if unitChanellingSpells then
             for _, spellSlot in pairs(unitChanellingSpells) do  --AUTO E
-                if TrundleMenu.Misc.ER:Value() and spell.name == GetCastName(unit, spellSlot) and GoS:ValidTarget(unit,GetCastRange(myHero,_E)) then 
-				 print("Want to E")
+if TrundleMenu.Misc.ER:Value() and spell.name == GetCastName(unit, spellSlot) and GoS:ValidTarget(unit,GetCastRange(myHero,_E)) then 
                   pos=GetOrigin(unit) --pos is a table!! :)
                   CastSkillShot(_E,pos.x,pos.y,pos.z)
-                end
-            end
-    end
-end) -- Marks the End of OnProcessSpell
-----------------------------------------------All my OnLoop code goes here----------------------------------------------
-
-OnLoop(function(myHero)
-AutoLevel()
-if IOW:Mode() == "Combo" then
-
-
-if TrundleMenu.Combo.Q:Value() and CanUseSpell(myHero,_Q) == READY and GoS:ValidTarget(target, 150) then
-CastSpell(_Q)
+		end
+	end
 end
 
-if TrundleMenu.Combo.W:Value() and CanUseSpell(myHero,_W) == READY and GoS:ValidTarget(target, 900) then
-CastSpell(_W)
-end
-
-EPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),math.huge,250,1000,225,false,false)
-
-if TrundleMenu.Combo.E:Value() and CanUseSpell(myHero,_E) == READY and GoS:ValidTarget(target, 1000) then
-CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
-end
-
-if TrundleMenu.Combo.R:Value() and CanUseSpell(myHero,_R) == READY and GoS:ValidTarget(target, 700) then 
-  tank=findTank() 
-  CastTargetSpell(tank,_R)
-end
-end
-end
-end)	--ends combo -- Four ifs
-	
 ----------------------------------------------All my Auto Level Code goes here----------------------------------------------
 function AutoLevel()
- if TrundleMenu.Misc.QE:Value() then
- 
-local level=GetLevel(myHero)
+if TrundleMenu.Misc.QE:value() then
 
 if level == 1 then
    LevelSpell(_Q)
@@ -133,10 +106,10 @@ if level == 1 then
    LevelSpell(_E)
  elseif level == 18 then
    LevelSpell(_E)
- end
- end
+      end
+      end
 
-if TrundleMenu.Misc.WQ:Value() then
+if TrundleMenu.Misc.WQ:value() then
     
  if level == 1 then
    LevelSpell(_Q)
@@ -174,23 +147,39 @@ if TrundleMenu.Misc.WQ:Value() then
    LevelSpell(_E)
  elseif level == 18 then
    LevelSpell(_E)
- end
-end
+    end
+    end
+---------------------------------------Combo IOW--------------------------------------
+function Combo()
+if IOW:Mode() == "Combo" then
+
+if TrundleMenu.Combo.Q:value() and CanUseSpell(,_Q) == READY and IsTargetable and GoS:ValidTarget(target, 150)
+then CastSpell(_Q)
 end
 
--- Function to find the highest resistance hero
+if TrundleMenu.Combo.W:Value() and CanUseSpell(,_W) == READY and GoS:ValidTarget(target, 900)
+then myHeroPos =GetOrigin(myHero) CastSkillShot(_W,myHeroPos.x,myHeroPos.y,myHeroPos.z)
+end
+
+if TrundleMenu.Combo.E:Value() and CanUseSpell(,_E) == READY and GoS:ValidTarget(target, 1000) and EPred.HitChance == 1 then CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
+end
+
+if TrundleMenu.Combo.R:Value() and CanUseSpell(,_R) == READY and GoS:ValidTarget(target, 700) then
+unit=findTank() 
+CastTargetedSpell(unit,_R)
+end
+
 function findTank()
   local besttank=0 --resistance
   local tank=nil --champ 
-  if GoS:GetEnemyHeroes() then --Can't be nil 
-   for _,k in pairs(GoS:GetEnemyHeroes()) do -- K means enemy just to remind myself
+  if GetEnemyHeroes() then --Can't be nil 
+   for _,k in pairs(GetEnemyHeroes()) do -- K means enemy just to remind myself
      if GetArmor(k)+GetMagicResist(k)>besttank and GoS:ValidTarget(target,GetCastRange(myHero,_R)) then
       besttank=GetArmor(k)+GetMagicResist(k)
       tank=k
      end
    end
   end
- print("Best Tank: "..GetObjectName(tank).." with "..besttank.." Points") --This can be removed. This is a Debug message
 return tank
 end
 
