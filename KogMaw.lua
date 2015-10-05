75831e1f6a1ed2ede4264492099472a2ef52735d
DarknessVoided <darknessvoided@icloud.com>
2015-10-05 14:14:32 +0800

    Update KogMaw.lua

--- KogMaw.lua
+++ KogMaw.lua
@@ -27,7 +27,6 @@ KogMawMenu.Combo:Boolean("R", "Use R", true)
 KogMawMenu.Combo:Key("Combo1", "Combo", string.byte(" "))
 
 KogMawMenu.Mana:Slider("Q", "Stop Q when % MP", 40, 1, 100, 1)
-KogMawMenu.Mana:Slider("W", "Stop W when % MP", 40, 1, 100, 1)
 KogMawMenu.Mana:Slider("E", "Stop E when % MP", 40, 1, 100, 1)
 KogMawMenu.Mana:Slider("R", "Stop R when % MP", 40, 1, 100, 1)
 
@@ -49,7 +48,7 @@ local Mana = (GetCurrentMana(myHero)/GetMaxMana(myHero))*100 --This will get the
         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 	end
 
-	if CanUseSpell(myHero,_W) == READY and KogMawMenu.Combo.Combo1:Value() and Mana >= KogMawMenu.Mana.W:Value() and KogMawMenu.Combo.W:Value() and GoS:ValidTarget(target, 600) then
+	if CanUseSpell(myHero,_W) == READY and KogMawMenu.Combo.Combo1:Value() and KogMawMenu.Combo.W:Value() and GoS:ValidTarget(target, 600) then
 		CastSpell(_W)
 	end
 
@@ -65,16 +64,20 @@ end) --Ends the OnLoop
 function KS()
 	local target = GetCurrentTarget()
 	for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+		local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(enemy),1650,250,1200,70,true,true)
+		local EPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(enemy),1400,250,1360,120,false,true)
+		local RPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(enemy),math.huge,1200,1800,150,false,false)
+		
 	if CanUseSpell(myHero,_Q) == READY and KogMawMenu.Misc.KSQ:Value() and GoS:ValidTarget(enemy, 1200) and GetCurrentHP(enemy) < GoS:CalcDamage(myHero, enemy, 0, (50*GetCastLevel(myHero,_Q) + 30+ 0.5*(GetBonusAP(myHero)))) then
-		CastTargetSpell(enemy, _Q)
+		CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 	end
 
 	if CanUseSpell(myHero,_E) == READY and KogMawMenu.Misc.KSE:Value() and GoS:ValidTarget(enemy, 1360) and GetCurrentHP(enemy) < GoS:CalcDamage(myHero, enemy, 0, (50*GetCastLevel(myHero,_E) + 10 + 0.7*(GetBonusAP(myHero)))) then
-		CastTargetSpell(enemy, _E)
+		CastSpell(_W)
 	end
 
 	if CanUseSpell(myHero,_R) == READY and KogMawMenu.Misc.KSR:Value() and GoS:ValidTarget(enemy, 1800) and GetCurrentHP(enemy) < GoS:CalcDamage(myHero, enemy, 0, (40*GetCastLevel(myHero,_R) + 40 + 0.5*(GetBaseDamage(myHero) + GetBonusDmg(myHero) + 0.3*(GetBonusAP(myHero)))),0) then
-		CastTargetSpell(enemy, _R)
+		CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
 	end
 end
 end -- Ends the FunctionKS
