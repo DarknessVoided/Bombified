c0e7daacb07f29adefc5191836956a433365b34c
DarknessVoided <darknessvoided@icloud.com>
2015-10-21 11:21:53 +0800

    Update BM2.0.lua

--- BM2.0.lua
+++ BM2.0.lua
@@ -3,14 +3,27 @@ local Menu = Menu("BM", "BM 2.0")
 Menu:SubMenu("rofl", "Settings")
 
 Menu.rofl:Boolean("Laugh", "Enable Emotes", true)
+Menu.rofl:Boolean("Always", "Always laugh", false)
 Menu.rofl:List("EmotesTable", "Use which Emotes", 1, {"Dance", "Laugh", "Taunt", "Joke"})
 
 OnLoop(function(myHero)
 Laugh()
+Always()
 end)
 
 function Laugh()
-   if IsDead(target) and Menu.Laugh.EmotesTable:value() then
+   if IsDead(target) and Menu.rofl.Laugh:value() then
+      if Menu.rofl.EmotesTable:Value() == 1 then Emoticon = {EMOTE_DANCE}
+      elseif Menu.rofl.EmotesTable:Value() == 2 then Emoticon = {EMOTE_LAUGH}
+      elseif Menu.rofl.EmotesTable:Value() == 3 then Emoticon = {EMOTE_TAUNT}
+      elseif Menu.rofl.EmotesTable:Value() == 4 then Emoticon = {EMOTE_JOKE}
+      end
+      CastEmote(Emoticon)
+   end
+end
+
+function Always()
+   if IOW:TimeToMove() and Menu.rofl.ALways:Value() then
       if Menu.rofl.EmotesTable:Value() == 1 then Emoticon = {EMOTE_DANCE}
       elseif Menu.rofl.EmotesTable:Value() == 2 then Emoticon = {EMOTE_LAUGH}
       elseif Menu.rofl.EmotesTable:Value() == 3 then Emoticon = {EMOTE_TAUNT}
