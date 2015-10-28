33cb90f61de78f84dcbab560f5313eb59e69908c
DarknessVoided <darknessvoided@icloud.com>
2015-10-28 23:56:39 +0800

    Update KogMaw.lua

--- KogMaw.lua
+++ KogMaw.lua
@@ -59,10 +59,10 @@ KogMawMenu.Items:Boolean("BoTrK", "Use Botrk", true)
 KogMawMenu.Items:Boolean("Cutlass", "Use Cutlass", true)
 KogMawMenu.Items:Slider("III", "Use BoTrk or Cutlass when Health", 40, 1, 100, 1)
 
-KogMawMenu:TargetSelector("ts", "Target Selector", DAMAGE_PHYSICAL, 1200, TARGET_LESS_CAST)
+--[[KogMawMenu:TargetSelector("ts", "Target Selector", DAMAGE_PHYSICAL, 1200, TARGET_LESS_CAST) ]]--
 
 OnTick(function(myHero)
-   local target = KogMawMenu.ts:GetTarget()
+   local target = GetCurrentTarget()
    origin = GetOrigin(target)
    local QPred = GetPredictionForPlayer(myHeroPos(),target,GetMoveSpeed(target),1650,250,1200,70,true,true)
    local EPred = GetPredictionForPlayer(myHeroPos(),target,GetMoveSpeed(target),1400,250,1360,120,false,true)
