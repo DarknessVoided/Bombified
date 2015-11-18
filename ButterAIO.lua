e444843d7f75af99bb4dbc093b689b3aedeeca96
DarknessVoided <darknessvoided@icloud.com>
2015-11-18 09:40:10 +0800

    Update ButterAIO.lua

--- ButterAIO.lua
+++ ButterAIO.lua
@@ -2,6 +2,7 @@
 V1.0 - Initial Commit]]--
 
 if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua - Go download it and save it in Common!") return end
+AutoUpdate("/DarknessVoided/Bombified/blob/master/ButterAIO.lua","/DarknessVoided/Bombified/blob/master/ButterAIO.Version","ButterAIO.lua",1)
 
 SupportedHero = {["Akali"] = true}
 Class = "Akali"
