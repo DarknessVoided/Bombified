fa3856144ff51067efc77b7ee3768516141652a2
DarknessVoided <darknessvoided@icloud.com>
2015-11-19 12:32:51 +0800

    Update ButterAIO.lua

--- ButterAIO.lua
+++ ButterAIO.lua
@@ -1,7 +1,11 @@
 --[[
-V1.0 - Initial Commit]]--
+V1.0 - Initial Commit
+V1.1 - Fixed some bugs]]--
 
 if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua - Go download it and save it in Common!") return end
+if not pcall( require, "DamageLib" ) then PrintChat("You are missing DamageLib.lua - Go download it and save it in Common!") return end
+if not pcall( require, "DeftLib" ) then PrintChat("You are missing DeftLib.lua - Go download it and save it in Common!") return end
+
 AutoUpdate("/DarknessVoided/Bombified/blob/master/ButterAIO.lua","/DarknessVoided/Bombified/blob/master/ButterAIO.Version","ButterAIO.lua",1)
 
 SupportedHero = {["Akali"] = true}
@@ -27,6 +31,7 @@ Akali.Combo:Boolean("Q", "Use Q", true)
 Akali.Combo:Boolean("W", "Use W immediately after R", true)
 Akali.Combo:Boolean("E", "Use E", true)
 Akali.Combo:Boolean("R", "Use R", true)
+Akali.Combo:Boolean("Dive", "Do not Dive under turret", true)
 
 Akali.Drawings:Boolean("Q", "Show Q range", false)
 Akali.Drawings;Boolean("WSpots", "Draw W Spots which gives extra range(For Escaping)", false)
@@ -52,11 +57,13 @@ function Akali:Combo()
         AttackUnit(target)
       end
       
-      if Ready(_E) and ValidTarget(target,300) and Akali.COmbo.Q:Value() then
+      if Ready(_E) and ValidTarget(target,300) and Akali.Combo.E:Value() then
         CastSpell(_E)
+		elseif Ready(_E) and ValidTarget(target,300) and not UnderTurret and Akali.Combo.Dive:Value() and Akali.Combo.E:value() then
+				CastSpell(_E)
       end
       
-      if Ready(_R) and ValidTarget(target) >= Akali.Combo.QW:Value() then
+      if Ready(_R) and ValidTarget(target) >= Akali.Combo.R:Value() then
         CastTargetSpell(_R,target)
       end
 end
