d83d91fb41dc02ebbc3b32c450840ee76a418871
DarknessVoided <darknessvoided@icloud.com>
2016-03-14 11:03:59 +0800

    Update Pantheon.lua

--- Pantheon.lua
+++ Pantheon.lua
@@ -8,7 +8,17 @@ end
 
 local version = 0.1
 
---AutoUpdate("/DarknessVoided/Bombified/master/Pantheon.lua)
+function AutoUpdate(data)
+    if tonumber(data) > tonumber(version) then
+        PrintChat("New version found! " .. data)
+        PrintChat("Downloading update, please wait...")
+        DownloadFileAsync("https://raw.githubusercontent.com/DarknessVoided/Bombified/master/Pantheon.lua", SCRIPT_PATH .. "Pantheon.lua", function() PrintChat("Update Completed, please 2x F6!") return end)
+    else
+        PrintChat("No updates found!")
+    end
+end
+
+GetWebResultAsync("https://raw.githubusercontent.com/DarknessVoided/Bombified/master/Pantheon.version", AutoUpdate)
 
 
 PrintChat("Panties loaded. Have a good game.")
@@ -51,7 +61,8 @@ PMenu.Misc:Boolean("AutoLevel", "Use AutoLevel", false)
 
 	OnTick(function(myHero)
 	local unit = GetCurrentTarget()
-		if not IsDead() then
+	local pI = GetConicAOEPrediction(unit, E)
+		if not IsDead(unit) then
 			Combo(unit)
 			KS(unit)
 			Item()
@@ -59,5 +70,41 @@ PMenu.Misc:Boolean("AutoLevel", "Use AutoLevel", false)
 		end
 	end
 
+--CallBacks
+OnUpdateBuff(function(unit,buffProc)
+	if unit == GetCurrentTarget() then
+		if buffProc.Type == 5 then
+			CC = true
+		elseif buffProc.Type == 8 then
+			CC = true
+		elseif buffProc.Type == 10 then
+			CC = true
+		end
+	end
+end)
+
 function Combo(unit)
-	if CanUseSpell(,_Q) and isAround
+	if IOW:Mode() == "Combo" then
+		if Ready(_Q) and ValidTarget(unit, 600) and PMenu.Harass.Q:Value() then
+			CastTargetSpell(unit, _Q)
+		end
+
+		if Ready(_W) and ValidTarget(unit, 600) and PMenu.Harass.W:Value() and if not CC then
+			CastTargetSpell(unit, _W)
+		end
+
+		if pI and pI.hitChance >= 0.25 and PMenu.Harass.E:Value() then
+			CastSkillShot(_E, pI.castPos)
+		end
+	end
+end
+
+function Harass(unit)
+	if IOW:Mode() == "Harass" then
+		if Ready(_Q) and ValidTarget(unit, 600) and PMenu.Harass.Q:Value() then
+			CastTargetSpell(unit, _Q)
+		elseif Ready(_Q) and ValudTarget(unit, 600) and PMenu.Harass.Q:Value() and PMenu.Harass.TQ:Value() and if not UnderTurret(unit, enemyTurret) then
+			CastTargetSpell(unit, _Q)
+		end
+	end
+end
