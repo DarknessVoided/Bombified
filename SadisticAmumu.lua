f980a77878ee9a35aeaa7c00118b856faf7000be
DarknessVoided <darknessvoided@icloud.com>
2016-04-05 20:55:45 +0800

    Update SadisticAmumu.lua

--- SadisticAmumu.lua
+++ SadisticAmumu.lua
@@ -1,1 +1,40 @@
-local ver = 0.01 --U got kek-ed
+local ver = 0.01
+
+if FileExist(COMMON_PATH.."MixLib.lua") then
+ require('MixLib')
+ LoadMixLib()
+else
+ PrintChat("MixLib not found. Please wait for download.")
+ DownloadFileAsync("https://raw.githubusercontent.com/VTNEETS/NEET-Scripts/master/MixLib.lua", COMMON_PATH.."MixLib.lua", function() PrintChat("Update Complete, please 2x F6!") return end)
+end
+
+function AutoUpdate(data)
+    if tonumber(data) > tonumber(version) then
+        PrintChat("New version found! " .. data)
+        PrintChat("Downloading update, please wait...")
+        DownloadFileAsync("https://raw.githubusercontent.com/DarknessVoided/Bombified/master/SadisticAmumu.lua", SCRIPT_PATH .. "SadisticAmumu.lua", function() PrintChat("Update Completed, please 2x F6!") return end)
+    else
+        PrintChat("No updates found!")
+    end
+end
+ 
+GetWebResultAsync("https://raw.githubusercontent.com/DarknessVoided/Bombified/master/SadisticAmumu.lua", AutoUpdate)
+
+local QData = { delay = 0.25, speed = 2000, width = 80, range = 1100, collision = true, coll = 1, aoe = false, type = "linear" }
+
+
+
+
+Sad = MenuConfig("Amumu", "Amumu")
+
+Sad:Menu("Combo", "Combo")
+Sad.Combo:Boolean("Q", "Use Q", true)
+Sad.Combo:Boolean("W", "Use W", true)
+Sad.Combo:Boolean("E", "Use E", true)
+Sad.Combo:Boolean("R", "Use R", true)
+Sad.Combo:Slider("RM", "R only when enemy more than", 3, 1, 5, 1)
+
+
+
+
+
