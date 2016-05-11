e7c3b9809a92891812f447b95f816f5af31a0b52
DarknessVoided <darknessvoided@icloud.com>
2016-05-11 20:25:53 +0800

    Update SadisticAmumu.lua

--- SadisticAmumu.lua
+++ SadisticAmumu.lua
@@ -1,36 +1,11 @@
-local version = "0.55"
+local version = "0.56"
 
-------------Callback-------------
---[[OnCreateObj(function(object)
-	if GetObjectBaseName(Object) == "Despair_buf.troy" then
-		WActive = true
-	end
-end)
-
-OnDeleteObj(object)
-	if GetObjectBaseName(function(Object) == "Despair_buf.troy" then
-		WActive = false
-	end)
-end ]]--
-
-OnUpdateBuff(function(unit,buff)
-	if unit == myHero and buff.Name == "despair" then
-		WActive = true
-	end
-end)
-
-OnRemoveBuff(function(unit,buff)
-	if unit == myHero and buff.Name == "despair" then
-		WActive = true
-	end
-end)
----------------------------
 if FileExist(COMMON_PATH.."MixLib.lua") then
  require('MixLib')
- LoadMixLib()
+ Mix:LoadPred()
 else
  PrintChat("MixLib not found. Please wait for download.")
- DownloadFileAsync("https://raw.githubusercontent.com/VTNEETS/NEET-Scripts/master/MixLib.lua", COMMON_PATH.."MixLib.lua", function() PrintChat("Update Complete, please 2x F6!") return end)
+ DownloadFileAsync("https://raw.githubusercontent.com/VTNEETS/NEET-Scripts/master/MixLib.lua", COMMON_PATH.."MixLib.lua", function() PrintChat("Downloaded MixLib, please 2x F6!") return end)
 end
 
 function AutoUpdate(data)
@@ -43,10 +18,11 @@ function AutoUpdate(data)
     end
 end
  
-GetWebResultAsync("https://raw.githubusercontent.com/DarknessVoided/Bombified/master/SadisticAmumu.lua", AutoUpdate)
+GetWebResultAsync("https://raw.githubusercontent.com/DarknessVoided/Bombified/master/SadisticAmumu.version", AutoUpdate)
 
 PrintChat("Thanks NEET for everything!")
 PrintChat("SadisticAmumu loaded!")
+------------------------------
 
 Sad = MenuConfig("Amumu", "Amumu")
 
@@ -63,37 +39,41 @@ Sad.LaneClear:Slider("NW", "W only when minion more than", 3, 1, 5, 1)
 Sad.LaneClear:Boolean("E", "Use E to laneclear", true)
 Sad.LaneClear:Slider("NE", "E only when minions more than", 3, 1, 5, 1)
 
+Sad:Menu("KillSteal", "KillSteal")
+Sad.KillSteal:Boolean("E", "Use E", true)
+Sad.KillSteal:Boolean("R", "Use R", false)
+
 Sad:Menu("Prediction", "Prediction")
 Sad.Prediction:Slider("Q", "Q Hit-Chance", 25, 1, 100, 1, function() UpdateHitChance() end)
 
 Sad:Menu("Misc", "Misc")
+Sad.Misc:Boolean("StopW", "Stop W", true)
 Sad.Misc:Boolean("RBlock", "Block R", true)
-Sad.Misc:DropDown("S", "Skin Changer", 1, {"Classic", "Pharaoh", "Vancouver", "Emumu", "Re-Gifted", "Almost-Prom King", "Little Knight", "Sad Robot", "Surprised Party", "Turn off"})
+Sad.Misc:DropDown("S", "Skin Changer", 1, {"Classic", "Pharaoh", "Vancouver", "Emumu", "Re-Gifted", "Almost-Prom King", "Little Knight", "Sad Robot", "Surprised Party"}, function() AmumuSkinsChange() end)
 
-local QData = Spells(_Q, 0.25, 2000, 80, 1100, true, 1, false, "linear", "Amumu Q", Sad.Prediction.Q:Value())
+local QData = Spells(_Q, 0.25, 2000, 80, myHero:GetSpellData(_Q).range, true, 1, false, "linear", "Amumu Q", Sad.Prediction.Q:Value()/100)
+local WActive = false
 
 function UpdateHitChance()
-	QData:GetHitChance(Sad.Prediction.Q:Value())
+	QData:GetHitChance(Sad.Prediction.Q:Value()/100)
 end
+
+function AmumuSkinsChange()
+	HeroSkinChanger(myHero, Sad.Misc.S:Value() - 1)
+end
+AmumuSkinsChange()
 
 OnTick(function(myHero)
 		Combo()
 		StopW()
-		laneclear()
-end)
-
-OnDraw(function(myHero)
-	if Sad.Misc.S:Value() ~= 10 then
-		HeroSkinChanger(myHero, Sad.Misc.S:Value() - 1)
-	elseif Sad.Misc.S:Value() == 10 then
-		HeroSkinChanger(myHero, 0)
-	end
+		Laneclear()
+        KillSteal()
 end)
 
 function Combo()
 local target = GetCurrentTarget()
 	if Mix:Mode() == "Combo" then
-		if ValidTarget(target, 1100) and Sad.Combo.Q:Value() and Ready(_Q) and target ~= nil then
+		if ValidTarget(target, 1100) and Sad.Combo.Q:Value() and Ready(_Q) then
 			QData:Cast1(target)
 		end
 		
@@ -111,31 +91,29 @@ local target = GetCurrentTarget()
 	end
 end
 
-function laneclear()
+function Laneclear()
 	if Mix:Mode() == "LaneClear" then
-		for _, mob in pairs(minionManager.objects) do
-			if mob ~= nil then
-				if Ready(_E) and ValidTarget(mob, 350) and MinionsAround(myHero, 300) >= Sad.LaneClear.NW:Value() and Sad.LaneClear.E:Value() then
-					CastSpell(_E)
-				end
+	    if Ready(_E) and Sad.LaneClear.W:Value() and MinionsAround(myHero.pos, 300, MINION_ENEMY) >= Sad.LaneClear.NE:Value() and Sad.LaneClear.E:Value() then
+	        CastSpell(_E)
+	    end
 			
-				if Ready(_W) and ValidTarget(mob, 300) and MinionsAround(myHero, 300) >= Sad.LaneClear.NE:Value() and WActive == false then
-					CastSpell(_W)
-				end
-			end
-		end
+	    if Ready(_W) and Sad.LaneClear.W:Value() and MinionsAround(myHero.pos, 300, MINION_ENEMY) >= Sad.LaneClear.NW:Value() and WActive == false then
+	        CastSpell(_W)
+	    end
 	end
 end
 
 function StopW()
-	if (EnemiesAround(myHero, 300) == 0 or MinionsAround(myHero, 300) == 0) and Ready(_W) and WActive == true then
-		CastSpell(_W)
-	end
+    if not Sad.Misc.StopW:Value() then return end
+    if not WActive then return end
+    if Mix:Mode() == "Combo" and EnemiesAround(myHero.pos, 300) < 1 then CastSpell(_W) end
+    if Mix:Mode() == "LaneClear" and MinionsAround(myHero.pos, 300, MINION_ENEMY) < 1 and MinionsAround(myHero.pos, 300, MINION_JUNGLE) < 1 then CastSpell(_W) end
+    if Mix:Mode() == "NotActive" and EnemiesAround(myHero.pos, 300) < 1 and MinionsAround(myHero.pos, 300, MINION_ENEMY) < 1 and MinionsAround(myHero.pos, 300, MINION_JUNGLE) < 1 then CastSpell(_W) end
 end
 
 function KillSteal()
-local EDmg = (25*(GetCastLevel(myHero, _E))+50)+((GetBaseDMG(myHero)+GetBonusAP(myHero))*0.5)
-local RDmg = (100*(GetCastLevel(myHero, _R))+50)+((GetBaseDMG(myHero)+GetBonusAP(myHero))*0.8)
+local EDmg = 25*GetCastLevel(myHero, _E) + 50 + myHero.ap*0.5
+local RDmg = 100*GetCastLevel(myHero, _R) + 50  + myHero.ap*0.8
 
 	for i,enemy in pairs(GetEnemyHeroes()) do
 		if Ready(_E) and Sad.KillSteal.E:Value() and ValidTarget(enemy, 350) and GetCurrentHP(enemy)+GetMagicShield(enemy) < CalcDamage(myHero, enemy, 0, EDmg) then
@@ -150,5 +128,17 @@ end
 OnSpellCast(function(spell)
 	if spell.spellID == _R and Sad.Misc.RBlock:Value() and EnemiesAround(myHero, 560) == 0 then
 		BlockCast()
+	end
+end)
+
+OnUpdateBuff(function(unit,buff)
+	if unit == myHero and buff.Name == "despair" then
+		WActive = true
+	end
+end)
+
+OnRemoveBuff(function(unit,buff)
+	if unit == myHero and buff.Name == "despair" then
+		WActive = false
 	end
 end)
